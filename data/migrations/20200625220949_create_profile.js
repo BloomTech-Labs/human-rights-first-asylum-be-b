feed5b129109685a2a3209f6cf8af253de161b92
Daniel Gamboa <danielgamboa10@gmail.com>
2021-05-18 11:27:46 -0500

    Refactor profiles table migration

--- data/migrations/20200625220949_create_profile.js
+++ data/migrations/20200625220949_create_profile.js
@@ -4,10 +4,10 @@ exports.up = (knex) => {
     .createTable('profiles', function (table) {
       table.string('id').notNullable().unique().primary();
       table.string('email').notNullable().unique();
-      table.string('firstName');
-      table.string('lastName');
+      table.string('first_name');
+      table.string('last_name');
       table.string('role').notNullable().default('user');
-      table.timestamps(true, true);
+      table.timestamps(false, true);
     });
 };
 
